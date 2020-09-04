d8843031dd526fbd81a8f351b52df8d79a6b4a9f
gianpiero <gianpiero.papappicco@be20.it>
2020-09-04 13:00:28 +0200

    Changed testcase name

--- dynamic/tests.py
+++ dynamic/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 # Create your tests here.
 class MapTestCase(TestCase):
 
-    def test_animals_can_speak(self):
+    def test_template(self):
         """Animals that can speak are correctly identified"""
         response = self.client.get('/map/')
         self.assertTemplateUsed(response, 'map.html')
