28a3692dfe755f261c796af74a481c1e77915baa
gianpiero <gianpiero.papappicco@be20.it>
2020-09-07 12:23:36 +0200

    Added project to pypi

---
+++ boot_django.py
@@ -0,0 +1,62 @@
+# boot_django.py
+#
+# This file sets up and configures Django. It's used by scripts that need to
+# execute as if running in a Django server.
+import os
+import django
+from django.conf import settings
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "dynamic"))
+
+
+def boot_django():
+    settings.configure(
+        BASE_DIR=BASE_DIR,
+        DEBUG=True,
+        TIME_ZONE="UTC",
+        USE_TZ=True,
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+            }
+        },
+        MIDDLEWARE=[
+            'django.middleware.security.SecurityMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.common.CommonMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        ],
+        INSTALLED_APPS=(
+            'django.contrib.admin',
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.messages',
+            'django.contrib.staticfiles',
+            'dynamic'
+        ),
+        STATIC_URL='/static/',
+        ROOT_URLCONF='dynamic.urls',
+        TEMPLATES=[
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [os.path.join(BASE_DIR, 'templates')]
+                ,
+                'APP_DIRS': True,
+                'OPTIONS': {
+                    'context_processors': [
+                        'django.template.context_processors.debug',
+                        'django.template.context_processors.request',
+                        'django.contrib.auth.context_processors.auth',
+                        'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ]
+
+    )
+    django.setup()
