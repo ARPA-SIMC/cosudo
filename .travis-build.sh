f60d35913f8af8daf4b3cdb153db6aee7a37ef50
gianpiero <gianpiero.papappicco@be20.it>
2020-09-09 12:09:38 +0200

    fixing travis

--- .travis-build.sh
+++ .travis-build.sh
@@ -3,11 +3,14 @@
 image=$1
 if [[ $image =~ ^centos:8 ]]; then
   dnf install -q -y epel-release
+  dnf install -q -y 'dnf-command(config-manager)'
+  dnf config-manager --set-enabled PowerTools
   dnf install -q -y python
   dnf install -q -y python-pip
-
+  pip3 install -r requirements.txt
+  python3 django-dynamic-map-borinud/load_tests.py
 elif [[ $image =~ ^fedora: ]]; then
   dnf install python3
+  pip install -r requirements.txt
+  python django-dynamic-map-borinud/load_tests.py
 fi
-pip3 install -r requirements.txt
-python3 django-dynamic-map-borinud/load_tests.py
