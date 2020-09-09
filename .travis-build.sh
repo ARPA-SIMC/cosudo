8103a2d1008e991b3ee1483461ebd9cc1be3a3c6
gianpiero <gianpiero.papappicco@be20.it>
2020-09-09 12:01:45 +0200

    fixing travis

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+image=$1
+if [[ $image =~ ^centos:8 ]]; then
+  dnf install -q -y epel-release
+  dnf install -q -y python
+  dnf install -q -y python-pip
+
+elif [[ $image =~ ^fedora: ]]; then
+  dnf install python3
+fi
 pip3 install -r requirements.txt
 python3 django-dynamic-map-borinud/load_tests.py
-
-
