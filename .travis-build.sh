b354bdccefa25e7408fee24cc1bfcee296a15b50
gianpiero <gianpiero.papappicco@be20.it>
2020-09-10 11:30:35 +0200

    Fixing copr

--- .travis-build.sh
+++ .travis-build.sh
@@ -2,14 +2,26 @@
 
 image=$1
 if [[ $image =~ ^centos:8 ]]; then
+  builddep="dnf builddep"
   dnf install -q -y epel-release
   dnf install -q -y 'dnf-command(config-manager)'
   dnf config-manager --set-enabled PowerTools
   dnf install -q -y python3 python3-pip
   pip3 install -r requirements.txt
   python3 django-dynamic-map-borinud/load_tests.py
+  dnf groupinstall -q -y "Development Tools"
+  dnf install -q -y 'dnf-command(builddep)'
+  dnf install -q -y rpmdevtools
+  dnf install -q -y pv
+  $builddep -q -y python-pysteps.spec
+  cd django-dynamic-map-borinud/
+  mkdir -p /rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+  cp django-dynamic-map-borinud.spec /rpmbuild/SPECS/
+  spectool -g -R -S ~/rpmbuild/SPECS/django-dynamic-map-borinud.spec
+  set +x
+  rpmbuild -ba /rpmbuild/SPECS/django-dynamic-map-borinud.spec 2>&1 | pv -q -L 3k
 elif [[ $image =~ ^fedora: ]]; then
-  dnf install q -y  python3 python3-pip
+  dnf install q -y python3 python3-pip
   pip3 install -r requirements.txt
   python3 django-dynamic-map-borinud/load_tests.py
 fi
