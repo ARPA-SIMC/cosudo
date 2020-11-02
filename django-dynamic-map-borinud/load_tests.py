5bc869c82438b6c014dea072304d7eca62a8b003
Gianpiero <gianpiero@pop-os.localdomain>
2020-11-02 10:44:32 +0100

    Added filters for attributes and bug fixing

--- django-dynamic-map-borinud/load_tests.py
+++ django-dynamic-map-borinud/load_tests.py
@@ -1,9 +1,11 @@
 import sys
 from unittest import TestSuite
 from boot_django import boot_django
+from django.core.management import call_command
 
 boot_django()
-
+call_command("makemigrations", "dynamic")
+call_command("migrate", "dynamic")
 default_labels = ["dynamic.tests", ]
 
 
